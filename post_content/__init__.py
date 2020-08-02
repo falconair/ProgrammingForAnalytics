90cb36b3343d0dcd2fdda9073c43a0181714116b
Shahbaz Chaudhary <shahbazc@gmail.com>
2020-08-01 19:54:35 -0500

    removes post_content code, it was for the old version

--- post_content/__init__.py
+++
@@ -1,8 +0,0 @@
-"""A Jupyter custom magic command to post cell contents to a REST server"""
-__version__ = '0.0.1'
-
-from .post_content import PostContent
-
-def load_ipython_extension(ipython):
-    print("PostContent loaded")
-    ipython.register_magics(PostContent)
