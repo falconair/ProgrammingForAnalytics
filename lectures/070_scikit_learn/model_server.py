4a29bf331b4c30c142a60d17a2827a51b98d2627
Shahbaz Chaudhary <shahbazc@gmail.com>
2021-03-20 21:46:00 -0500

    post class cleanup and adds some code to model_server

--- lectures/070_scikit_learn/model_server.py
+++ lectures/070_scikit_learn/model_server.py
@@ -1,6 +1,8 @@
 from flask import Flask, request
-
 from joblib import load
+
+import numpy as np
+import pandas as pd
 
 # Load trained model
 trained_model = load('model.joblib')
@@ -16,6 +18,23 @@ def hello_world():
 @app.route('/model')
 def serve_model():
     args = request.args
+    
     print(args)
-    print(args.get('pclass'))
-    return args
+
+    pclass, age, sibsp, parch, fare, adult_male, alone = float(args.get('pclass')), float(args.get('age')), float(args.get('sibsp')), float(args.get('parch')), float(args.get('fare')), float(args.get('adult_male')), float(args.get('alone'))
+
+    print(pclass, age, sibsp, parch, fare, adult_male, alone)
+    
+    input_array = np.array([[pclass, age, sibsp, parch, fare, adult_male, alone]])
+    
+    print(input_array)
+    
+    predicted = trained_model.predict(input_array)
+    return args, predicted
+
+if __name__ == "__main__":
+    app.run()  
+    
+# Test as:
+# http://localhost:5000/model?pclass=1&age=15&sibsp=0&parch=1&fare=211.3375&adult_male=0&alone=0
+# 1.    ,  15.    ,   0.    ,   1.    , 211.3375,   0.    ,0.
